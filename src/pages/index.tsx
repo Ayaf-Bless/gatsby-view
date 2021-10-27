433bde449a9717b97331f5b468132dc623a60e6f
ayaf-bless <blessambel1@gmail.com>
2021-10-28 02:20:56 +0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const IndexPage = () => <Wrapper>Hello</Wrapper>;
+const Wrapper = styled.div``;
+
+export default IndexPage;
